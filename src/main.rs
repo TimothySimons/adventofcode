8c8c3e0b12fc26d7714ec1cd668ea43a7e40cb86
Timothy Simons <timothy.simons@oracle.com>
2023-04-28 15:43:28 +0200

    Add day 2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod day01;
+mod day02;
 
 fn main() {
     println!("Day 01 part 1: {}", day01::part1("resources/day01.txt"));
     println!("Day 01 part 2: {}", day01::part2("resources/day01.txt"));
+
+    println!("Day 02 part 1: {}", day02::part1("resources/day02.txt"));
+    println!("Day 02 part 1: {}", day02::part2("resources/day02.txt"));
 }
